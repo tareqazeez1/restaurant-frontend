03f9d2e1b6b552ffcd7cf6fa5ab016e6babc91e3
tareqazeez1 <tareqazeez1@gmail.com>
2021-08-10 14:39:23 +0600

    Navbar added

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './style.NavBar.css';
+import { Nav, Navbar } from 'react-bootstrap';
+
+const NavBar = () => {
+    return (
+        <div>
+            <Navbar bg="dark" variant="dark"
+                sticky="top" expand="sm" collapseOnSelect>
+                <Navbar.Brand>
+                    Logo
+                </Navbar.Brand>
+
+                <Navbar.Toggle className="coloring" />
+                <Navbar.Collapse>
+                    <Nav>
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#blog">Blog</Nav.Link>
+                        <Nav.Link href="#about-us">About Us</Nav.Link>
+                        <Nav.Link href="#contact-us">Contact Us</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+
+            </Navbar>
+        </div>
+    );
+};
+
+export default NavBar;
